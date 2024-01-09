303d55b3ac6f1552a4dd34f8c459745be6cb335c
georgepletea <pleteageorge494@gmail.com>
2024-01-09 19:08:30 +0200

    first commit

---
+++ src/main/java/org/example/repository/entities/CarParts.java
@@ -0,0 +1,28 @@
+package org.example.repository.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Entity(name = "car_parts")
+
+    public class CarParts{
+            @Id
+            @GeneratedValue
+            private UUID id;
+            @NonNull
+            private double price;
+            @NonNull
+            private String name;
+            @NonNull
+}
+
+
---
+++ src/main/java/org/example/repository/entities/Offer.java
@@ -0,0 +1,29 @@
+package org.example.repository.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity(name = "offers")
+public class Offer {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @NonNull
+    @Column(name = "client_id")
+    private UUID clientid;
+    @NonNull
+    @Column(name = "car_parts_id")
+    private UUID carpartsId;
+    @NonNull
+    private Double amount;
+}
