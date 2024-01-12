b2ca65e1d39593d8239d38465bce82f3a8faa1ad
marlucov <lucov.marinescu@gmail.com>
2024-01-12 17:23:05 +0200

    Started populating database

---
+++ src/main/java/org/example/utils/PopulateCarPartsNames.java
@@ -0,0 +1,34 @@
+package org.example.utils;
+
+import org.example.config.app.Application;
+import org.example.repository.CarPartNameRepository;
+import org.example.repository.entities.CarPartName;
+import org.hibernate.Session;
+
+public class PopulateCarPartsNames {
+  public static void main(String[] args) {
+    Application.start();
+    deleteExisting(Application.getSession());
+    populate(Application.getSession());
+    Application.terminate();
+  }
+
+  public static void deleteExisting(Session session) {
+    CarPartNameRepository carPartNameRepository = new CarPartNameRepository(session);
+    carPartNameRepository.findAll().stream().map(CarPartName::getId).forEach(carPartNameRepository::delete);
+  }
+
+  public static void populate(Session session) {
+    CarPartNameRepository carPartNameRepository = new CarPartNameRepository(session);
+    carPartNameRepository.insert(new CarPartName("driver left seat"));
+    carPartNameRepository.insert(new CarPartName("driver right seat"));
+    carPartNameRepository.insert(new CarPartName("passenger right seat"));
+    carPartNameRepository.insert(new CarPartName("passenger left seat"));
+    carPartNameRepository.insert(new CarPartName("driver left rearview mirror"));
+    carPartNameRepository.insert(new CarPartName("driver right rearview mirror"));
+    carPartNameRepository.insert(new CarPartName("passenger right rearview mirror"));
+    carPartNameRepository.insert(new CarPartName("passenger left rearview mirror"));
+    carPartNameRepository.insert(new CarPartName("inside rearview mirror"));
+    carPartNameRepository.insert(new CarPartName("wheel"));
+  }
+}
