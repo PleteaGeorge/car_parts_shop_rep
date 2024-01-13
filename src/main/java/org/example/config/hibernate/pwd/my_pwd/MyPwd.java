86f2516974ba310cb2b6e75f970c7ff80da50909
marlucov <80281355+marlucov@users.noreply.github.com>
2024-01-13 02:13:15 +0200

    Delete MyPwd.java; scuze ca am intervenit brutal, dar ti-ai dezvaluit parola; eroare in .gitignore, linia 8 -> /**/

--- src/main/java/org/example/config/hibernate/pwd/my_pwd/MyPwd.java
+++
@@ -1,8 +0,0 @@
-package org.example.config.hibernate.pwd.my_pwd;
-
-class MyPwd {
-    private static final String PWD = "Vulc@noid01";
-    public static String gewPwd() {
-        return PWD;
-    }
-}
