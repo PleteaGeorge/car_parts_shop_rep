426436bc9d0794105bd4e5ee427d991639d59771
marlucov <lucov.marinescu@gmail.com>
2024-01-13 17:40:29 +0200

    Prepared PopulateDatabase for testing

--- src/main/java/org/example/utils/populate_database/PopulateDatabase.java
+++ src/main/java/org/example/utils/populate_database/PopulateDatabase.java
@@ -6,18 +6,20 @@ public class PopulateDatabase {
   public static void main(String[] args) {
     Application.start();
     //
+    PopulateClients.deleteExisting(Application.getSession());
+    PopulateClients.populate(Application.getSession());
+    //
     PopulateCars.deleteExisting(Application.getSession());
+    PopulateCars.populate(Application.getSession());
+    //
     PopulateCarPartsNames.deleteExisting(Application.getSession());
     //
+//    PopulateCarParts.deleteExisting(Application.getSession());
 //    PopulateCars.deleteExisting(Application.getSession());
 //    PopulateCars.populate(Application.getSession());
-//    PopulateCarParts.deleteExisting(Application.getSession());
 //    PopulateCarPartsNames.deleteExisting(Application.getSession());
 //    PopulateCarPartsNames.populate(Application.getSession());
 //    PopulateCarParts.populate(Application.getSession());
-//    //
-//    PopulateClients.deleteExisting(Application.getSession());
-//    PopulateClients.populate(Application.getSession());
     //
     Application.terminate();
   }
