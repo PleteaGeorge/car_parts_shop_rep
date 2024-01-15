9a4ee1f6408b7a6843cf979791826763e6f0332f
Alina <alinaiftode4@gmail.com>
2024-01-15 21:36:10 +0200

    Practical Project

--- src/main/java/org/example/repository/Repository.java
+++ src/main/java/org/example/repository/Repository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 public abstract class Repository<EntityT> {
-    @Getter
+
     private final Session session;
     private final Class<EntityT> entityClass;
 
