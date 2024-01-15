aa3b57d85959b0432a478ddf10fca2852fbe7b15
marlucov <lucov.marinescu@gmail.com>
2024-01-15 23:22:09 +0200

    Project general stub

--- src/main/java/org/example/utils/OfferUtility.java
+++ src/main/java/org/example/utils/OfferUtility.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public class OfferUtility {
   public static Double getTotalAmount(List<CarPart> carParts) {
-    return carParts.stream().map(CarPart::getPrice).reduce(.0, Double::sum);
+    return .0;
   }
 
   public static Double getDiscount(Offer offer) {
-    return 100 * (1 - (offer.getAmount() / getTotalAmount(offer.getCarParts())));
+    return .0;
   }
 
   public static Double getAmount(Double total, Double discount) {
-    return total * (1 - (discount / 100));
+    return .0;
   }
 }
